9ae43b39b22b46401c080363845dde22b187129d
Timmeey86 <Timmeey86@users.noreply.github.com>
2024-08-23 09:51:59 +0200

    always show log

--- scripts/HeightTypeDebugger.lua
+++ scripts/HeightTypeDebugger.lua
@@ -56,29 +56,29 @@ end
 
 Mission00.loadMission00Finished = Utils.appendedFunction(Mission00.loadMission00Finished, function(screen)
     local maxTypes = 2^g_densityMapHeightManager.heightTypeNumChannels - 1
-    if counter > maxTypes then
-        local userText = (g_i18n:getText("user_text_intro") .. g_i18n:getText("user_text_intro_two") .. "\r\n\r\n"):format(counter, maxTypes + 1)
-        print(">>>>>>> START HEIGHT TYPE DEBUG <<<<<<<")
-        for _, xmlFileMapping in pairs(heightTypeMapping) do
-            print("> File: " .. xmlFileMapping.xmlFile)
-            local fileCounter = 0
-            for _, data in pairs(xmlFileMapping.data) do
-                if data.alreadyExists then
-                    print((">>> %s: Ignored since it already exists"):format(data.name))
-                elseif not data.failed then
-                    print((">>> %s: Added on index %d"):format(data.name, data.index))
-                    fileCounter = fileCounter + 1
-                else
-                    print((">>> %s: Type exceeds maximum number of height types (index %d)"):format(data.name, data.index))
-                    fileCounter = fileCounter + 1
-                end
+    local userText = (g_i18n:getText("user_text_intro") .. g_i18n:getText("user_text_intro_two") .. "\r\n\r\n"):format(counter, maxTypes + 1)
+    print(">>>>>>> START HEIGHT TYPE DEBUG <<<<<<<")
+    for _, xmlFileMapping in pairs(heightTypeMapping) do
+        print("> File: " .. xmlFileMapping.xmlFile)
+        local fileCounter = 0
+        for _, data in pairs(xmlFileMapping.data) do
+            if data.alreadyExists then
+                print((">>> %s: Ignored since it already exists"):format(data.name))
+            elseif not data.failed then
+                print((">>> %s: Added on index %d"):format(data.name, data.index))
+                fileCounter = fileCounter + 1
+            else
+                print((">>> %s: Type exceeds maximum number of height types (index %d)"):format(data.name, data.index))
+                fileCounter = fileCounter + 1
             end
-            local modName = getModName(xmlFileMapping.xmlFile)
-            userText = ("%s%s: %s\r\n"):format(userText, modName, g_i18n:getText("user_text_height_types"):format(fileCounter))
         end
-        userText = userText .. "\r\n" .. g_i18n:getText("user_text_outro"):format(counter - maxTypes - 1, (maxTypes + 1) * 2)
-        print(">>>>>>> END HEIGHT TYPE DEBUG <<<<<<<")
+        local modName = getModName(xmlFileMapping.xmlFile)
+        userText = ("%s%s: %s\r\n"):format(userText, modName, g_i18n:getText("user_text_height_types"):format(fileCounter))
+    end
+    userText = userText .. "\r\n" .. g_i18n:getText("user_text_outro"):format(counter - maxTypes - 1, (maxTypes + 1) * 2)
+    print(">>>>>>> END HEIGHT TYPE DEBUG <<<<<<<")
 
+    if counter > maxTypes then
         g_currentMission.hud:showInGameMessage("", userText, -1, nil, nil, nil)
     end
 end)
